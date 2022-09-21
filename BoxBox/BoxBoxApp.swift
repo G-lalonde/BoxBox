9cf794bacd10c0182ae5555076dec45860c4c5fb
Jérémy Lalonde <jeremy.lalonde.1@ens.etsmtl.ca>
2022-09-21 11:21:21 -0400

    Initial Commit

---
+++ BoxBox/BoxBoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoxBoxApp.swift
+//  BoxBox
+//
+//  Created by Jérémy Lalonde on 2022-09-21.
+//
+
+import SwiftUI
+
+@main
+struct BoxBoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
